bfc47c62303abb72e739b5d60339633d4226e26b
Tabriz-13th <xx013xx@gmail.com>
2022-04-07 18:44:57 +0400

    DI v2.1

---
+++ Intellij/dependency-injection/src/main/java/az/spring/demo/Employee.java
@@ -0,0 +1,50 @@
+package az.spring.demo;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- Intellij/dependency-injection/src/main/java/az/spring/demo/Notification.java
+++ Intellij/dependency-injection/src/main/java/az/spring/demo/Notification.java
@@ -2,13 +2,23 @@ package az.spring.demo;
 
 public class Notification {
     private Message message;
+    private Employee employee;
 
-    public void setMessage(Message message) {
+
+    //contructor injection
+    public Notification(Message message, Employee employee) {
         this.message = message;
+        this.employee = employee;
     }
+
+//    public void setMessage(Message message) {
+//        this.message = message;
+//    }
 
     public void alert() {
         System.out.println("Notification...");
+        System.out.println(employee);
+
         message.send();
     }
 }
